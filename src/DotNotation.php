042d0d0a12d4cc4759ed100052312fcf762b3a81
e200 <eleandro@inbox.ru>
2017-10-12 23:48:39 +0100

    First commit

---
+++ src/DotNotation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Unity\Notator;
+
+use Unity\Contracts\Notator\INotation;
+
+/**
+ * Class DotNotation.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
+class DotNotation implements INotation
+{
+    /**
+     * Denotes a string using dot (.) as separator
+     *
+     * @param $notation
+     *
+     * @return string[]
+     */
+    static function denote($notation)
+    {
+        return explode('.', $notation);
+    }
+}
