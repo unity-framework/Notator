fbd3505a1ace194f8c7797c668bd2e786ce31f6e
e200 <eleandro@inbox.ru>
2017-11-02 13:56:54 +0100

    Added Notator

---
+++ src/Notator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Unity\Notator;
+
+use Unity\Contracts\Notator\INotator;
+
+/**
+ * Abstract class Notator.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ * @link   https://github.com/e200/
+ */
+abstract class Notator implements INotator
+{
+    /**
+     * Denotes a string using `static::SEPARATOR` as separator.
+     *
+     * @param string $notation
+     *
+     * @return string[]
+     */
+    public function denote($notation)
+    {
+        return explode(static::SEPARATOR, $notation);
+    }
+}
